82792c3ba5aa9f7bc436c92f5f8ad244e66a7bbf
haddanreda <haddan.reda.net@gmail.com>
2022-02-28 20:11:49 +0100

    first commit

---
+++ app/src/main/java/com/example/mediaplayer1/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.mediaplayer1
+
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+    class MainActivity : AppCompatActivity() {
+
+    private lateinit var mediaPlayer: MediaPlayer
+    val arrayList = ArrayList<String>()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView : RecyclerView = findViewById(R.id.recyclerView)
+        val adaptor = Adaptor(fetchData())
+        recyclerView.layoutManager = GridLayoutManager(this,3)
+        recyclerView.adapter = adaptor
+    }
+
+    private fun fetchData() : ArrayList<String> {
+        val item = ArrayList<String>()
+        for (i in 0 until 30) {
+            item.add("$i")
+        }
+        return item
+    }
+}
